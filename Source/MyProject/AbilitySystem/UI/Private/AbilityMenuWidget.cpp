c7ec92786d81600d3b45752a329d83dee4ba85ed
nathankeyt <nake8894@colorado.edu>
2024-02-13 14:36:47 -0700

    ability menu widget

---
+++ Source/MyProject/AbilitySystem/UI/Private/AbilityMenuWidget.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyProject/AbilitySystem/UI/Public/AbilityMenuWidget.h"
+
+
---
+++ Source/MyProject/AbilitySystem/UI/Public/AbilityMenuWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UAbilityMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+};
